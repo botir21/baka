72ab9b0979b94b636b71522f76a869b30381f897
Komiljonsan <90968768+Komiljonsan@users.noreply.github.com>
2022-11-11 18:33:41 +0500

    Add files via upload
    main files

---
+++ script.js
@@ -0,0 +1,9 @@
+function openNav() {
+    document.getElementById("mySidenav").style.width = "100px";
+    document.getElementById("X").style.transform = "rotate(180deg)"
+  }
+
+function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+    document.getElementById("X").style.transform = "rotate(360deg)"
+}
